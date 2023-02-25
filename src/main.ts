6992334913ab3ae6dc87e0f9084c512608f2d8d9
CharelMarais <maraischarel@gmail.com>
2023-02-25 18:53:02 +0200

    refactor: :zap: Implemented observables and refactored code

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,11 @@
-import {
-  addEventListenerToFlightInfoButtons,
-  appendFlightInformationToFlightInfoContainer,
-  minimiseLoadingScreen,
-  removeOldOutOfScopeFlightInfoRow,
-} from "./dom-manipulation.js";
+import { minimiseLoadingScreen } from "./dom-manipulation.js";
 import { IFlights } from "./models/flight.js";
-import { fetchStream$, pollStream$ } from "./services/flight-service.js";
+import { fetchStream$ } from "./services/flight-service.js";
 
 fetchStream$.subscribe({
-  next: (flight: IFlights[]): void => getAPIResponseAndUpdatePage(flight),
+  next: (flights: IFlights[]): void => {
+    localStorage.setItem("flights", JSON.stringify(flights));
+  },
   complete: () => minimiseLoadingScreen(),
 });
-
-pollStream$.subscribe((flight: IFlights[]): void => {
-  getAPIResponseAndUpdatePage(flight);
-});
-
-export function getAPIResponseAndUpdatePage(flight: IFlights[]): void {
-  const currentFlightCodes = [];
-  for (let flightInfo of flight) {
-    appendFlightInformationToFlightInfoContainer(flightInfo);
-    currentFlightCodes.push(flightInfo.icao24);
-  }
-  addEventListenerToFlightInfoButtons(flight);
-  removeOldOutOfScopeFlightInfoRow(currentFlightCodes);
-}
+minimiseLoadingScreen();
